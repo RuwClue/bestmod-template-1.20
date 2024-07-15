004af98e1865dfcbdfa55d84a1f207d0d5bd9a1b
Clueloss <80123408+Clueloss@users.noreply.github.com>
2024-07-15 17:38:22 -0400

    Initial commit

---
+++ src/main/java/net/ruwayd/bestmod/BestModClient.java
@@ -0,0 +1,10 @@
+package net.ruwayd.bestmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
